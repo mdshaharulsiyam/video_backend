a60ea61b885a47f5f08fa5963d19a7ea983f2bdd
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2025-11-25 21:57:11 +0600

    s

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: 'http://localhost:5173',
+    credentials: true,
+  }
+));
 app.use(express.json());
 
 let SHORTS = [
